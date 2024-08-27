59142ccc004eee6c61d7508c96a0deb1a113c0f5
Anubhav <choubey.anubhav253@gmail.com>
2024-08-27 13:29:00 +0530

    Simplified code for JS

--- script.js
+++ script.js
@@ -5,11 +5,4 @@ const myDiv = document.getElementById('myDiv');
 // Add an event listener for the 'click' event
 myDiv.addEventListener('click', function () {
     console.log('Div was clicked!');
-    // You can perform other actions here when the div is clicked
-});
-
-// Optional: Add an event listener for the 'mousedown' event
-myDiv.addEventListener('mousedown', function () {
-    console.log('Div is being pressed!');
-    // You can perform other actions here when the div is pressed
 });
