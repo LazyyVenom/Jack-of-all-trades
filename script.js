b587e3da2f2c7eb01f7b4a9cb34ef1c5d16d0268
Anubhav <choubey.anubhav253@gmail.com>
2024-08-29 09:49:23 +0530

    Added JS to add click and Card show effect

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const jackCardsTrigger = document.getElementById('jack_cards_trigger');
 const jackCards = document.getElementById('jack_cards');
+const cards = document.querySelectorAll(".jack_of_all_trades .card");
 
 jackCardsTrigger.addEventListener('click', function () {
     jackCards.classList.add('animate');
@@ -11,22 +12,26 @@ document.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
         jackCards.classList.remove('animate');
     }
+    cards.forEach(card => {
+        const cardFlip = card.querySelector(".card_flip");
+        if (cardFlip.style.display == "flex") {
+            cardFlip.classList.add('animate');
+            cardFlip.style.display = "none";
+        }
+    });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const cards = document.querySelectorAll(".jack_of_all_trades .card");
-
     cards.forEach(card => {
         card.addEventListener("click", function () {
             const cardFlip = this.querySelector(".card_flip");
-
-            if (cardFlip && cardFlip.style.display == "flex") {
+            if (cardFlip.style.display == "flex") {
+                cardFlip.classList.add('animate');
                 cardFlip.style.display = "none";
             }
             else {
-                if (cardFlip) {
-                    cardFlip.style.display = "flex";
-                }
+                cardFlip.classList.add('animate');
+                cardFlip.style.display = "flex";
             }
         });
     });
